106e84cdf0bf6b239e7b7ba6050e7e2149f89400
KiranA9303 <43024303+KiranA9303@users.noreply.github.com>
2019-10-30 20:20:17 -0400

    code changes for android

--- app/src/main/java/com/example/foodapplication/MainActivity.java
+++ app/src/main/java/com/example/foodapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.foodapplication;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,8 +12,16 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText username, password;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        /*addListenerOnLoginButton();
+        EditText txtname1 = findViewById(R.id.username);
+        String username      =  txtname1.getText().toString();
+        EditText txtname2 = findViewById(R.id.username);
+        String password      =  txtname2.getText().toString();
+        Button loginButton = (Button)findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });*/
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -51,5 +73,19 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void addListenerOnLoginButton() {
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        loginButton = (Button) findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String value1=username.getText().toString();
+                String value2=password.getText().toString();
+                Toast.makeText(getApplicationContext(),String.valueOf( username +","+password), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
